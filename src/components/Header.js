0d174066ba8be138160da4a1ed5626f316f32796
salih <salihkilinc80@gmail.com>
2023-01-26 11:07:37 +0100

    v2

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,54 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { Container, Navbar, Nav } from 'react-bootstrap'
 import { ThemeContext } from '../GlobalComponents/ThemeProvider'
-import {BiSun , BiMoon ,BiCart } from "react-icons/bi"
+import { BiSun, BiMoon, BiCart } from "react-icons/bi"
+import { Link } from "@reach/router"
+import {useCart} from "react-use-cart"
 //import { ThemeConsumer ,ThemeContext } from 'react-bootstrap/esm/ThemeProvider'
 
 const Header = () => {
-    const { theme, setThemeMode } = useContext(ThemeContext); 
+    const { theme, setThemeMode } = useContext(ThemeContext);
     const [darkMode, setDarkMode] = useState(theme);
 
 
-useEffect(()=>{
-    setThemeMode(darkMode)
-},[darkMode])
+    useEffect(() => {
+        setThemeMode(darkMode)
+        console.log(darkMode)
+    },[darkMode])
+
+    const {
+        isEmpty,
+        totalItems,
+    }=useCart();
 
     return (
         <div>
             <Navbar collapseOnSelect expand="md"
-            variant={darkMode? "dark": "light"}
-            className={darkMode? "bg-light-black border-bottom": "bg-light border-bottom" }
-            style={{width:"100%" , position: "fixed" , zIndex:100}}
+                variant={darkMode ? "dark" : "light"}
+                className={darkMode ? "bg-light-black border-bottom" : "bg-light border-bottom"}
+                style={{ width: "100%", position: "fixed", zIndex: 100 }}
             >
                 <Container>
-                    <Navbar.Brand className={darkMode? "text-dark-primary": "text-light-primary"} ><b>Simple-ecart </b> </Navbar.Brand>
+                    <Link to="/">
+                        <Navbar.Brand className={darkMode ? "text-dark-primary" : "text-light-primary"} ><b>Simple-ecart </b> </Navbar.Brand>
+
+                    </Link>
+
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto">
-                            <Nav.Link className={darkMode? "text-dark-primary": "text-light-primary"} 
-                            onClick={()=>setDarkMode(!darkMode)}
-                            > {darkMode? <BiSun size="1.7rem"/>: <BiMoon size="1.7rem"/>}   </Nav.Link>  
-                            <Nav.Link href="#link">Link</Nav.Link>
+                            <Nav.Link className={darkMode ? "text-dark-primary" : "text-light-primary"}
+                                onClick={() => setDarkMode(!darkMode)}
+                            > {darkMode ? <BiSun size="1.7rem" /> : <BiMoon size="1.7rem" />}   </Nav.Link>
+
+
+                            <Link to="/cart" className={`  ${darkMode ? "text-dark-primary" : "text-light-primary"}  d-flex align-items-center`}
+                            >
+                                <BiCart size="2rem" />
+                                {!isEmpty && <span style={{position :"relative", left:"-21px", top:"-18px" }} >{totalItems}</span>}
+                                <span style={{marginLeft: !isEmpty ? "-13px": 0 }} >Cart</span>
+                                
+                            </Link>
 
                         </Nav>
                     </Navbar.Collapse>
@@ -39,3 +59,5 @@ useEffect(()=>{
 }
 
 export default Header
+// Nav LIn icerisine dark ve light mode verdik bunuda iconlar ile onclick hale getirdik
+// 
